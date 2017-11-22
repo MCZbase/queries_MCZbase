9cded07bdcd7f5c35b76acc020dbe214c3d143ec
chicoreus <mole@morris.net>
2017-11-22 15:04:54 -0500

    ISSUE: Redmine 353  PURPOSE:  adding another country qc query. DESCRIPTION: to country_qc file.

--- geography/country_qc.sql
+++ geography/country_qc.sql
@@ -3,3 +3,14 @@
 select distinct g.country 
     from geog_auth_rec g left join ctcountry_code c on g.country = c.country 
     where c.country is null and g.country not like '%/%';
+
+--  List of country/continent combinations where a country occurs in more than one continent
+select distinct country, continent_ocean from geog_auth_rec where country in (
+select country from geog_auth_rec 
+    where country is not null and continent_ocean not like '%Ocean%'
+    group by country 
+    having count(distinct continent_ocean) > 1
+)
+and continent_ocean not like '%Ocean%'
+order by country
+;
