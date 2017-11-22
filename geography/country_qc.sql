57e3408295eb0c18f57468296839e15bce95814a
chicoreus <mole@morris.net>
2017-11-22 14:26:40 -0500

    ISSUE: Redmine 353 PURPOSE: Testing adding a report query to the repository. DESCRIPTION: Adding a file containing a geography quality control report query.

---
+++ geography/country_qc.sql
@@ -0,0 +1,4 @@
+--  List of distinct country names in the higher geography table that aren't in the country code authority file,
+--  and aren't lists of countries separated by a slash (convention when current country is one of a set).
+select distinct g.country from geog_auth_rec g left join ctcountry_code c on g.country = c.country 
+where c.country is null and g.country not like '%/%';
